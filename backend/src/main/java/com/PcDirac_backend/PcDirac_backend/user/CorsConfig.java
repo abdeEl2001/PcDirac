28c4ef3ae04da0bfe607d797157576c8931750e2
abdelhadi imtki <imtkiabdelhadi@gmail.com>
2025-09-16 16:39:06 +0100

    change cors configuration 1

--- backend/src/main/java/com/PcDirac_backend/PcDirac_backend/user/CorsConfig.java
+++ backend/src/main/java/com/PcDirac_backend/PcDirac_backend/user/CorsConfig.java
@@ -1,16 +1,13 @@
 package com.PcDirac_backend.PcDirac_backend.user;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
 import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
@@ -28,3 +25,4 @@ public class CorsConfig {
         return new CorsFilter(source);
     }
 }
+
