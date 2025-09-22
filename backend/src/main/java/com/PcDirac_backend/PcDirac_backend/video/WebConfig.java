0d76d9d77136793c1df1cf68ee757474873b1dc2
abdelhadi imtki <imtkiabdelhadi@gmail.com>
2025-09-22 20:30:28 +0100

    version updated of website 4

---
+++ backend/src/main/java/com/PcDirac_backend/PcDirac_backend/video/WebConfig.java
@@ -0,0 +1,15 @@
+package com.PcDirac_backend.PcDirac_backend.video;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/var/www/PcDirac/backend/uploads/");
+    }
+}
+
