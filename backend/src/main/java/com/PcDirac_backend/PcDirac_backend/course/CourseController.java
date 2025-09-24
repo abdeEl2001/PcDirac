87ae49f65059316e44e9b343a6aa1926c9999155
abdelhadi imtki <imtkiabdelhadi@gmail.com>
2025-09-24 16:27:53 +0100

    update cross origin

--- backend/src/main/java/com/PcDirac_backend/PcDirac_backend/course/CourseController.java
+++ backend/src/main/java/com/PcDirac_backend/PcDirac_backend/course/CourseController.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
