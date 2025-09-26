f7c3b956f44b01460e321c65727f1e2b1b997c47
abdelhadi imtki <imtkiabdelhadi@gmail.com>
2025-09-26 16:37:04 +0100

    do the props concept 8

--- pcdirac/src/pages/Agregation.js
+++ pcdirac/src/pages/Agregation.js
@@ -7,8 +7,10 @@ export default function Agregation() {
       endpoint="/api/courses/etudiant/agregation"
       title="Agrégation"
       filters={[
-        { key: "matiere", label: "Toutes les matières" },
-        { key: "professeur", label: "Tous les professeurs" }
+        { key: "categorie", label: "Tous les catégories", defaultValue: "Concours d'entrée" },
+        { key: "matiere", label: "Toutes les matières", defaultValue:"Physique" },
+        { key: "titre", label: "Tous les titres", defaultValue: "" },
+        { key: "professeur", label: "Tous les professeurs" },
       ]}
       buttonLabels={{
         "Cours": "Voir le cours",
