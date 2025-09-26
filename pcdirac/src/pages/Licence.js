db5cf0ea40c6efd684882036f9dd12022b629fca
abdelhadi imtki <imtkiabdelhadi@gmail.com>
2025-09-26 16:47:11 +0100

    do the props concept 12

--- pcdirac/src/pages/Licence.js
+++ pcdirac/src/pages/Licence.js
@@ -1,4 +1,5 @@
 // Licence.js
+import { defaultValueTypes } from "framer-motion";
 import FilesPage from "./FilesPage";
 
 export default function Licence() {
@@ -7,8 +8,8 @@ export default function Licence() {
       endpoint="/api/courses/etudiant/license"
       title="Licence"
       filters={[
-        { key: "categorie", label: "Toutes les catégories" },
-        { key: "matiere", label: "Toutes les matières" },
+        { key: "categorie", label: "Toutes les catégories",defaultValue:"Cours Licence" },
+        { key: "matiere", label: "Toutes les matières" ,defaultValue:"Physique"},
         { key: "unite", label: "Toutes les unités", defaultValue: "" },
         { key: "titre", label: "Tous les titres", defaultValue: "" }
       ]}
